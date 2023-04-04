f6dcbb38499d0eb77a754753e750bd8cbb6e0fe4
lmalexander <42302324+lmalexander@users.noreply.github.com>
2023-04-04 13:37:30 -0400

    top tile row color pop

--- index.js
+++ index.js
@@ -9,8 +9,16 @@ function nextTile() {
 };
 
 function colorTile () {
-activeTile = "62"
-    console.log(activeTile)
-    document.getElementById(activeTile).setAttribute("class" , "green-line")
+    
+    document.getElementById("29").setAttribute("class" , "indigo-line");
+    document.getElementById("13").setAttribute("class" , "green-line");
+    document.getElementById("62").setAttribute("class" , "black-line");
+    document.getElementById("9").setAttribute("class" , "green-line");
+    document.getElementById("20").setAttribute("class" , "blue-line");
+    document.getElementById("5").setAttribute("class" , "yellow-line");
+    document.getElementById("41").setAttribute("class" , "red-line");
+    document.getElementById("38").setAttribute("class" , "violet-line");
+
+
 
 };
