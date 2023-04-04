61135ec4b0ae2a9113b48fbdee39f0e9286566b4
lmalexander <42302324+lmalexander@users.noreply.github.com>
2023-04-03 22:54:31 -0400

    color tile animation on tile 62 with onclick

---
+++ index.js
@@ -0,0 +1,16 @@
+console.log("index.js is connected")
+
+let activeTile = ""; 
+
+function nextTile() {
+
+    
+
+};
+
+function colorTile () {
+activeTile = "62"
+    console.log(activeTile)
+    document.getElementById(activeTile).setAttribute("class" , "green-line")
+
+};
