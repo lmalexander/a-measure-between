5cb9c0fd87fc15468fd3161f34f491fcc1b4d9b2
lmalexander <42302324+lmalexander@users.noreply.github.com>
2023-04-04 17:23:07 -0400

    returns 64 cards & change color w/ colorTile

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 console.log("i.js is connected")
 
-let testGrid = [
+let testGridOld = [
     {
         id: 1,
         line: "yellow-line",
@@ -15,17 +15,16 @@ let testGrid = [
 
 ];
 
-
-
-
-
-
-
-
-
-
-
-
+let testGrid = [
+    1, 2, 3, 4, 5, 6, 7, 8,
+    9, 10, 11, 12, 13, 14, 15, 16,
+    17, 18, 19, 20, 21, 22, 23, 24,
+    25, 26, 27, 28, 29, 30, 31, 32,
+    33, 34, 35, 36, 37, 38, 39, 40,
+    41, 42, 43, 44, 45, 46, 47, 48,
+    49, 50, 51, 52, 53, 54, 55, 56,
+    57, 58, 59, 60, 61, 62, 63, 64
+];
 
 let testWords = [
     {
@@ -55,8 +54,8 @@ function tileGrid() {
         // create card + assign classes & ID
         let tile = document.createElement("div");
         tile.className = "card tile text-center";
-        tile.setAttribute("id", testGrid[i].id);
-        console.log(testGrid[i].id);
+        tile.setAttribute("id", testGrid[i]);
+        console.log(testGrid[i]);
     
         // create card-body div
         let tileBody = document.createElement("div");
@@ -91,7 +90,15 @@ tileGrid();
 
 
 function colorTile () {    
-    document.getElementById("1").setAttribute("class" , "indigo-line");
+    document.getElementById("1").setAttribute("class" , "yellow-line");
+    document.getElementById("9").setAttribute("class" , "green-line");
+    document.getElementById("17").setAttribute("class" , "blue-line");
+    document.getElementById("25").setAttribute("class" , "indigo-line");
+    document.getElementById("33").setAttribute("class" , "violet-line");
+    document.getElementById("41").setAttribute("class" , "red-line");
+    document.getElementById("49").setAttribute("class" , "orange-line");
+    document.getElementById("57").setAttribute("class" , "black-line");
+
 };
 
 
