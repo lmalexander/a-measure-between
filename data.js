c4972840055cea7896a5781de1d29f395711d5e7
lmalexander <42302324+lmalexander@users.noreply.github.com>
2023-04-07 16:51:58 -0400

    organized word data arrays by part of speech

--- data.js
+++ data.js
@@ -1,45 +1,45 @@
 console.log("data.js is connected")
 
-let ambData = [
+let ambDataNoun = [
     {
-        word: "black",
-        wordPOS: "adjective",
+        word: "white",
+        wordPOS: "noun",
         wordSyl: 1,
     },
     {
-        word: "pynk",
-        wordPOS: "adjective",
-        wordSyl: 1
-    },
-    {
-        word: "D4L",
-        wordPOS: "acronym",
-        wordSyl: 3,
-    },
-    {
-        word: "addicting",
-        wordPOS: "adjective",
-        wordSyl: 3,
-    },
-    {
-        word: "all-white",
-        wordPOS: "adjective",
+        word: "coast",
+        wordPOS: "noun",
         wordSyl: 1,
     },
     {
-        word: "American",
-        wordPOS: "adjective",
-        wordSyl: 1,
-    },
-    {
-        word: "Alright",
-        wordPOS: "interjection",
+        word: "body",
+        wordPOS: "noun",
         wordSyl: 2,
     },
     {
-        word: "ass",
+        word: "calendar",
+        wordPOS: "noun",
+        wordSyl: 3,
+    },
+    {
+        word: "love",
         wordPOS: "noun",
         wordSyl: 1,
+    },
+    {
+        word: "caliber",
+        wordPOS: "noun",
+        wordSyl: 3,
+    },
+    {
+        word: "reason",
+        wordPOS: "noun",
+        wordSyl: 2,
+    },
+    {
+        word: "valley",
+        wordPOS: "noun",
+        wordSyl: 2,
     },
     {
         word: "bottle",
@@ -47,9 +47,428 @@ let ambData = [
         wordSyl: 2,
     },
     {
-        word: "bando",
+        word: "mind",
+        wordPOS: "noun",
+        wordSyl: 1,
+    },
+    {
+        word: "thighs",
+        wordPOS: "noun",
+        wordSyl: 1,
+    },
+    {
+        word: "whip",
+        wordPOS: "noun",
+        wordSyl: 1,
+    },
+    {
+        word: "mirror",
         wordPOS: "noun",
         wordSyl: 2,
     },
+    {
+        word: "college",
+        wordPOS: "noun",
+        wordSyl: 2,
+    },
+    {
+        word: "closet",
+        wordPOS: "noun",
+        wordSyl: 2,
+    },
+    {
+        word: "thang",
+        wordPOS: "noun",
+        wordSyl: 1,
+    },
+    {
+        word: "America's",
+        wordPOS: "noun",
+        wordSyl: 4,
+    },
+    {
+        word: "enemy",
+        wordPOS: "noun",
+        wordSyl: 3,
+    },
+    {
+        word: "skin",
+        wordPOS: "noun",
+        wordSyl: 1,
+    },
+    {
+        word: "bando",
+        wordPOS: "noun",
+        wordSyl: 2,
+    }
 ];
 
+let ambDataVerb = [
+    {
+        word: "came",
+        wordPOS: "verb",
+        wordSyl: 1,
+    },
+    {
+        word: "hangin",
+        wordPOS: "verb",
+        wordSyl: 2,
+    },
+    {
+        word: "talkin",
+        wordPOS: "verb",
+        wordSyl: 2,
+    },
+    {
+        word: "smell",
+        wordPOS: "verb",
+        wordSyl: 1,
+    },
+    {
+        word: "matter",
+        wordPOS: "verb",
+        wordSyl: 2,
+    },
+    {
+        word: "tell",
+        wordPOS: "verb",
+        wordSyl: 1,
+    },
+    {
+        word: "finna",
+        wordPOS: "verb",
+        wordSyl: 2,
+    },
+    {
+        word: "acting",
+        wordPOS: "verb",
+        wordSyl: 2,
+    },
+    {
+        word: "going",
+        wordPOS: "verb",
+        wordSyl: 2,
+    },
+    {
+        word: "show",
+        wordPOS: "verb",
+        wordSyl: 1,
+    },
+    {
+        word: "dancing",
+        wordPOS: "verb",
+        wordSyl: 2,
+    },
+    {
+        word: "mouth",
+        wordPOS: "verb",
+        wordSyl: 1,
+    },
+    {
+        word: "tryna",
+        wordPOS: "verb",
+        wordSyl: 2,
+    },
+    {
+        word: "climb",
+        wordPOS: "verb",
+        wordSyl: 1,
+    },
+    {
+        word: "meant",
+        wordPOS: "verb",
+        wordSyl: 1,
+    },
+    {
+        word: "woo",
+        wordPOS: "verb",
+        wordSyl: 1,
+    },
+    {
+        word: "sayin",
+        wordPOS: "verb",
+        wordSyl: 2,
+    },
+    {
+        word: "go",
+        wordPOS: "verb",
+        wordSyl: 1,
+    },
+    {
+        word: "pop",
+        wordPOS: "verb",
+        wordSyl: 1,
+    },
+];
+
+let ambDataAdj = [
+    {
+        word: "ninety-nine",
+        wordPOS: "adjective",
+        wordSyl: 4,
+    },
+    {
+        word: "slow",
+        wordPOS: "adjective",
+        wordSyl: 1,
+    },
+    {
+        word: "butt-naked",
+        wordPOS: "adjective",
+        wordSyl: 2,
+    },
+    {
+        word: "better",
+        wordPOS: "adjective",
+        wordSyl: 2,
+    },
+    {
+        word: "free",
+        wordPOS: "adjective",
+        wordSyl: 1,
+    },
+    {
+        word: "American",
+        wordPOS: "adjective",
+        wordSyl: 4,
+    },
+    {
+        word: "different",
+        wordPOS: "adjective",
+        wordSyl: 3,
+    },
+    {
+        word: "easy",
+        wordPOS: "adjective",
+        wordSyl: 2,
+    },
+    {
+        word: "lil'",
+        wordPOS: "adjective",
+        wordSyl: 1,
+    },
+    {
+        word: "prepared",
+        wordPOS: "adjective",
+        wordSyl: 2,
+    },
+    {
+        word: "blacked-out",
+        wordPOS: "adjective",
+        wordSyl: 3,
+    },
+    {
+        word: "handcuffed",
+        wordPOS: "adjective",
+        wordSyl: 2,
+    },
+    {
+        word: "posterior",
+        wordPOS: "adjective",
+        wordSyl: 4,
+    },
+    {
+        word: "toxic",
+        wordPOS: "adjective",
+        wordSyl: 2,
+    },
+    {
+        word: "harder",
+        wordPOS: "adjective",
+        wordSyl: 2,
+    },
+    {
+        word: "lotta",
+        wordPOS: "adjective",
+        wordSyl: 2,
+    },
+    {
+        word: "hot",
+        wordPOS: "adjective",
+        wordSyl: 1,
+    },
+    {
+        word: "crazy",
+        wordPOS: "adjective",
+        wordSyl: 2,
+    },
+    {
+        word: "favorite",
+        wordPOS: "adjective",
+        wordSyl: 3,
+    }
+];
+
+let ambDataAdv = [
+    {
+        word: "kinda",
+        wordPOS: "adverb",
+        wordSyl: 2,
+    },
+    {
+        word: "when",
+        wordPOS: "adverb",
+        wordSyl: 1,
+    },
+    {
+        word: "whenever",
+        wordPOS: "adverb",
+        wordSyl: 3,
+    },
+    {
+        word: "out",
+        wordPOS: "adverb",
+        wordSyl: 1,
+    },
+    {
+        word: "here",
+        wordPOS: "adverb",
+        wordSyl: 1,
+    },
+    {
+        word: "how",
+        wordPOS: "adverb",
+        wordSyl: 1,
+    },
+    {
+        word: "still",
+        wordPOS: "adverb",
+        wordSyl: 1,
+    },
+    {
+        word: "more",
+        wordPOS: "adverb",
+        wordSyl: 1,
+    },
+    {
+        word: "just",
+        wordPOS: "adverb",
+        wordSyl: 1,
+    },
+    {
+        word: "why",
+        wordPOS: "adverb",
+        wordSyl: 1,
+    },
+    {
+        word: "wrong",
+        wordPOS: "adverb",
+        wordSyl: 1,
+    },
+    {
+        word: "never",
+        wordPOS: "adverb",
+        wordSyl: 2,
+    },
+    {
+        word: "down",
+        wordPOS: "adverb",
+        wordSyl: 1,
+    },
+    {
+        word: "much",
+        wordPOS: "adverb",
+        wordSyl: 1,
+    },
+    {
+        word: "up",
+        wordPOS: "adverb",
+        wordSyl: 1,
+    },
+    {
+        word: "no",
+        wordPOS: "adverb",
+        wordSyl: 1,
+    },
+    {
+        word: "ever",
+        wordPOS: "adverb",
+        wordSyl: 2,
+    },
+    {
+        word: "too",
+        wordPOS: "adverb",
+        wordSyl: 1,
+    },
+    {
+        word: "now",
+        wordPOS: "adverb",
+        wordSyl: 1,
+    },
+    {
+        word: "back",
+        wordPOS: "adverb",
+        wordSyl: 1,
+    },
+];
+
+let ambDataIntj = [
+    {
+        word: "alright",
+        wordPOS: "interjection",
+        wordSyl: 2,
+    },
+    {
+        word: "damn",
+        wordPOS: "interjection",
+        wordSyl: 1,
+    },
+    {
+        word: "uh",
+        wordPOS: "interjection",
+        wordSyl: 1,
+    },
+    {
+        word: "nigga",
+        wordPOS: "interjection",
+        wordSyl: 2,
+    },
+    {
+        word: "yuh",
+        wordPOS: "interjection",
+        wordSyl: 1,
+    },
+    {
+        word: "yeah",
+        wordPOS: "interjection",
+        wordSyl: 1,
+    },
+    {
+        word: "mm",
+        wordPOS: "interjection",
+        wordSyl: 1,
+    }
+];
+
+let ambDataPunct = [
+    {
+        word: ".",
+        wordPOS: "punctuation",
+        wordSyl: 1,
+    },
+    {
+        word: "!",
+        wordPOS: "punctuation",
+        wordSyl: 1,
+    },
+    {
+        word: "+",
+        wordPOS: "punctuation",
+        wordSyl: 1,
+    },
+    {
+        word: "?",
+        wordPOS: "punctuation",
+        wordSyl: 1,
+    },
+    {
+        word: ";",
+        wordPOS: "punctuation",
+        wordSyl: 1,
+    },
+    {
+        word: ":",
+        wordPOS: "punctuation",
+        wordSyl: 1,
+    },
+]; 
