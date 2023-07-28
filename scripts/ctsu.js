a39dfcea41e93380a99779a5f07e7f92bb80cde5
EMA <elizabeth.alexander@northwestern.edu>
2023-07-28 07:25:20 -0400

    added adv, verb, & intj functions

--- scripts/ctsu.js
+++ scripts/ctsu.js
@@ -1,11 +1,31 @@
 console.log("ctsu.js is connected")
 
 let subjBtnText = document.getElementById("subject-btn-text");
+let advBtnText = document.getElementById("adverb-btn-text");
+let verbBtnText = document.getElementById("verb-btn-text");
+let intjBtnText = document.getElementById("intj-btn-text");
+
 
 function subjBtn() {
     let subjID = Math.floor(Math.random() * 20) + 1;
-
     subjBtnText.innerText = ambDataNoun[subjID].word;
-
 }
 subjBtn();
+
+function advBtn() {
+    let advID = Math.floor(Math.random() * 19) + 1;
+    advBtnText.innerText = ambDataAdv[advID].word;
+}
+advBtn();
+
+function verbBtn() {
+    let verbID = Math.floor(Math.random() * 19) + 1;
+    verbBtnText.innerText = ambDataVerb[verbID].word;
+}
+verbBtn();
+
+function intjBtn() {
+    let intjID = Math.floor(Math.random() * 6) + 1;
+    intjBtnText.innerText = ambDataIntj[intjID].word;
+}
+intjBtn();
