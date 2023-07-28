541156f25ed9731bd0020260665d0b992d998598
EMA <elizabeth.alexander@northwestern.edu>
2023-07-28 07:48:33 -0400

    added WASD keydown event listeners

--- scripts/ctsu.js
+++ scripts/ctsu.js
@@ -5,27 +5,52 @@ let advBtnText = document.getElementById("adverb-btn-text");
 let verbBtnText = document.getElementById("verb-btn-text");
 let intjBtnText = document.getElementById("intj-btn-text");
 
+document.addEventListener("keydown", function(Event) {
+    if (Event.key == "w") {
+        subjBtn();
+        console.log("w worked");
+    }
+});
+
+document.addEventListener("keydown", function(Event) {
+    if (Event.key == "a") {
+        advBtn();
+        console.log("a worked");
+    }
+});
+
+document.addEventListener("keydown", function(Event) {
+    if (Event.key == "s") {
+        verbBtn();
+        console.log("s worked");
+    }
+});
+
+document.addEventListener("keydown", function(Event) {
+    if (Event.key == "d") {
+        intjBtn();
+        console.log("d worked");
+    }
+});
+
 
 function subjBtn() {
     let subjID = Math.floor(Math.random() * 20) + 1;
     subjBtnText.innerText = ambDataNoun[subjID].word;
-}
-subjBtn();
+};
 
 function advBtn() {
     let advID = Math.floor(Math.random() * 19) + 1;
     advBtnText.innerText = ambDataAdv[advID].word;
-}
-advBtn();
+};
 
 function verbBtn() {
     let verbID = Math.floor(Math.random() * 19) + 1;
     verbBtnText.innerText = ambDataVerb[verbID].word;
-}
-verbBtn();
+};
 
 function intjBtn() {
     let intjID = Math.floor(Math.random() * 6) + 1;
     intjBtnText.innerText = ambDataIntj[intjID].word;
-}
-intjBtn();
+};
+
