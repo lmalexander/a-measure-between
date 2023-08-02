0788919844025eb65dacb2df1ffaa5b2c8e40063
EMA <elizabeth.alexander@northwestern.edu>
2023-08-01 23:33:16 -0400

    added lordeAudio, switched data src to papa parse

--- scripts/ctsu.js
+++ scripts/ctsu.js
@@ -1,56 +1,110 @@
 console.log("ctsu.js is connected")
 
-let subjBtnText = document.getElementById("subject-btn-text");
-let advBtnText = document.getElementById("adverb-btn-text");
-let verbBtnText = document.getElementById("verb-btn-text");
-let intjBtnText = document.getElementById("intj-btn-text");
+
+// papa parse data
+let ambDataGSheetURL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSWh26h65bH4-gzqIehi16G05IIVmsLKK0xyg9SE4BjqodEtAkfoUKD-vNAg_3tcDsvKN_NGXUvhVhV/pub?gid=0&single=true&output=csv";
+
+let dataVerbsArray = [];
+let dataNounsArray = [];
+let dataAdjArray = [];
+
+// > papaparse CSV to JSON pull
+Papa.parse(ambDataGSheetURL, {
+    download: true,
+    header: true,
+    complete: showData,
+});
+
+
+// > data return function 
+function showData(result) {
+    let rawData = result.data;
+    // console.log(rawData)
+
+    for (let index = 0; index < rawData.length; index++) {
+        
+        if (rawData[index].partOfSpeech === "noun") {
+             dataNounsArray.push(rawData[index].word);
+        } else if (rawData[index].partOfSpeech === "verb") {
+             dataVerbsArray.push(rawData[index].word)
+        } else if (rawData[index].partOfSpeech === "adjective") {
+             dataAdjArray.push(rawData[index].word)
+        }
+    }
+};
+
+let ambLordeGSheetURL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSWh26h65bH4-gzqIehi16G05IIVmsLKK0xyg9SE4BjqodEtAkfoUKD-vNAg_3tcDsvKN_NGXUvhVhV/pub?gid=1897655938&single=true&output=csv";
+let lordeFilepathArray = [];
+
+Papa.parse(ambLordeGSheetURL, {
+    download: true,
+    header: true,
+    complete: showLorde,
+});
+
+function showLorde(result) {
+    let rawLordeData = result.data;
+
+    for (let index = 0; index < rawLordeData.length; index++) {
+        lordeFilepathArray.push(rawLordeData[index]);
+        
+    }
+}
+
+
+let verbText = document.getElementById("verb-text");
+let adjBtnText = document.getElementById("adj-text");
+let nounText = document.getElementById("noun-text");
+let lordeAudio = document.getElementById("lorde-audio");
+
+
+
+function verbTextReturn() {
+    let verbID = Math.floor(Math.random() * 48) + 1;
+    verbText.innerText = dataVerbsArray[verbID];
+};
+
+function adjTextReturn() {
+    let adjID = Math.floor(Math.random() * 13) + 1;
+    adjText.innerText = dataAdjArray[adjID];
+};
+
+function nounTextReturn() {
+    let nounID = Math.floor(Math.random() * 48) + 1;
+    nounText.innerText = dataNounsArray[nounID];
+};
+
+function lordeAudioReturn() {
+    let lordeID = Math.floor(Math.random() * 6) + 1;
+    lordeAudio.setAttribute("src" , lordeFilepathArray[lordeID]);
+}
+
+
 
 document.addEventListener("keydown", function(Event) {
     if (Event.key == "w") {
-        subjBtn();
-        console.log("w worked");
+        verbTextReturn();
+        console.log("w returned a verb");
     }
 });
 
 document.addEventListener("keydown", function(Event) {
     if (Event.key == "a") {
-        advBtn();
-        console.log("a worked");
+        nounTextReturn();
+        console.log("a returned a verb");
     }
 });
 
 document.addEventListener("keydown", function(Event) {
     if (Event.key == "s") {
-        verbBtn();
-        console.log("s worked");
+        adjTextReturn;
+        console.log("s returned an adjective");
     }
 });
 
 document.addEventListener("keydown", function(Event) {
     if (Event.key == "d") {
-        intjBtn();
+        lordeAudioReturn();
         console.log("d worked");
     }
 });
-
-
-function subjBtn() {
-    let subjID = Math.floor(Math.random() * 20) + 1;
-    subjBtnText.innerText = ambDataNoun[subjID].word;
-};
-
-function advBtn() {
-    let advID = Math.floor(Math.random() * 19) + 1;
-    advBtnText.innerText = ambDataAdv[advID].word;
-};
-
-function verbBtn() {
-    let verbID = Math.floor(Math.random() * 19) + 1;
-    verbBtnText.innerText = ambDataVerb[verbID].word;
-};
-
-function intjBtn() {
-    let intjID = Math.floor(Math.random() * 6) + 1;
-    intjBtnText.innerText = ambDataIntj[intjID].word;
-};
-
