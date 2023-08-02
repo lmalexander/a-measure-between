f587eb3efcb669e2bfc0437b6e0d4ad12ba4bb5a
EMA <elizabeth.alexander@northwestern.edu>
2023-08-01 21:47:39 -0400

    changed poem line structure, reduced to 34 tiles

--- scripts/index.js
+++ scripts/index.js
@@ -7,32 +7,28 @@ let tileGridIDs = [
     1, 2, 3, 4, 5, 6, 7, 8,
     9, 10, 11, 12, 13, 14, 15, 16,
     17, 18, 19, 20, 21, 22, 23, 24,
-    25, 26, 27, 28, 29, 30, 31, 32,
-    33, 34, 35, 36, 37, 38, 39, 40,
-    41, 42, 43, 44, 45, 46, 47, 48,
-    49, 50, 51, 52, 53, 54, 55, 56,
-    57, 58, 59, 60, 61, 62, 63, 64
+    25, 26, 27, 28, 29, 30, 31, 32
 ];
 
 // tile part of speech ID arrays
-let aTiles = [1, 9, 17, 25, 33, 41, 49, 57];
-let verbTiles = [2, 10, 18, 26, 34, 42, 50, 58];
-let betweenTiles = [3, 11, 19, 27, 35, 43, 51, 59];
-let nounOneTiles = [4, 12, 20, 28, 36, 44, 52, 60];
-let andTiles = [5, 13, 21, 29, 37, 45, 53, 61];
-let nounTwoTiles = [6, 14, 22, 30, 38, 46, 54, 62];
-let intjTiles = [7, 15, 23, 31, 39, 47, 55, 63];
-let breakTiles = [8, 16, 24, 32, 40, 48, 56, 64];
+let aTiles = [1, 9, 17, 25];
+let verbTiles = [2, 10, 18, 26];
+let betweenTiles = [3, 11, 19, 27];
+let youTiles = [4, 12, 20, 28];
+let andTiles = [5, 13, 21, 29];
+let yourTiles = [6, 14, 22, 30];
+let adjTiles = [7, 15, 23, 31];
+let nounTiles = [8, 16, 24, 32];
 
 // tile color ID arrays
-let yellowTiles = [1, 2, 3, 4, 5, 6, 7, 8];
-let greenTiles = [9, 10, 11, 12, 13, 14, 15, 16];
-let blueTiles = [17, 18, 19, 20, 21, 22, 23, 24];
-let indigoTiles = [25, 26, 27, 28, 29, 30, 31, 32];
-let violetTiles = [33, 34, 35, 36, 37, 38, 39, 40];
-let redTiles = [41, 42, 43, 44, 45, 46, 47, 48];
-let orangeTiles = [49, 50, 51, 52, 53, 54, 55, 56];
-let blackTiles = [57, 58, 59, 60, 61, 62, 63, 64];
+let yellowTiles = [1, 2, 3, 4];
+let greenTiles = [5, 6, 7, 8];
+let blueTiles = [9, 10, 11, 12];
+let indigoTiles = [13, 14, 15, 16];
+let violetTiles = [17, 18, 19, 20];
+let redTiles = [21, 22, 23, 24];
+let orangeTiles = [25, 26, 27, 28];
+let blackTiles = [29, 30, 31, 32];
 
 // variables
 let i = 1;
@@ -50,10 +46,13 @@ function createGrid() {
         let tileCol = document.createElement("div");
         tileCol.className = "col";
     
-        // create card + assign classes & ID
+        // create card button + assign classes & ID
         let tile = document.createElement("div");
-        tile.className = "card tile text-center";
+        tile.className = "card tile text-center btn";
         tile.setAttribute("id", tileGridIDs[index]);
+        tile.setAttribute("type", "button");
+        tile.setAttribute("data-bs-toggle" , "modal");
+        tile.setAttribute("data-bs-target" , "#modal-" + [index]);
     
         // create card-body div
         let tileBody = document.createElement("div");
@@ -64,19 +63,9 @@ function createGrid() {
         let pTileWord = document.createElement("p");
         pTileWord.className = "tile-word";
         pTileWord.setAttribute("id" , "tw-" + tileGridIDs[index])
-    
-        // create tile-syllables p
-        //let pTileSyllable = document.createElement("p");
-        //pTileSyllable.className = "tile-syllable";
-        //pTileSyllable.setAttribute("id" , "ts-" + tileGridIDs[index])
 
-        // create break btn
-        let btnBreakDiv = document.createElement("div");
-        btnBreakDiv.setAttribute("id" , "btn-break-div-" + tileGridIDs[index]);
     
         // append card elements to card
-      //  tileBody.appendChild(pTileSyllable);
-        tileBody.appendChild(btnBreakDiv);
         tileBody.appendChild(pTileWord);
         tile.appendChild(tileBody);
     
@@ -95,10 +84,9 @@ createGrid();
 // papa parse data
 let ambGSheetURL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSWh26h65bH4-gzqIehi16G05IIVmsLKK0xyg9SE4BjqodEtAkfoUKD-vNAg_3tcDsvKN_NGXUvhVhV/pub?gid=0&single=true&output=csv";
 
-// let rawDataArray = [];
 let dataVerbsArray = [];
 let dataNounsArray = [];
-let dataIntjArray = [];
+let dataAdjArray = [];
 
 // > papaparse CSV to JSON pull
 Papa.parse(ambGSheetURL, {
@@ -111,9 +99,7 @@ Papa.parse(ambGSheetURL, {
 // > data return function 
 function showData(result) {
     let rawData = result.data;
-    console.log(rawData)
-    
-  
+    // console.log(rawData)
 
     for (let index = 0; index < rawData.length; index++) {
         
@@ -121,32 +107,18 @@ function showData(result) {
              dataNounsArray.push(rawData[index].word);
         } else if (rawData[index].partOfSpeech === "verb") {
              dataVerbsArray.push(rawData[index].word)
-        } else if (rawData[index].partOfSpeech === "interjection") {
-             dataIntjArray.push(rawData[index].word)
+        } else if (rawData[index].partOfSpeech === "adjective") {
+             dataAdjArray.push(rawData[index].word)
         }
     }
-
-
-console.log(dataNounsArray);
-console.log(dataVerbsArray);
-console.log(dataIntjArray);
-
-
-
-
-
 };
 
 
+function popGrid() {
 
-
-
-       function popGrid() {
-
-   // console.log(rawData)
     console.log(dataNounsArray);
-console.log(dataVerbsArray);
-console.log(dataIntjArray);
+    console.log(dataVerbsArray);
+    console.log(dataAdjArray);
 
 
     tileGridIDs.forEach( i => {
@@ -155,11 +127,8 @@ console.log(dataIntjArray);
 
         // every 2 seconds, return word, ID, & color to tiles based on ID:
             let activeNounID = Math.floor(Math.random() * 48) + 1;
-            let activeIntjID = Math.floor(Math.random() * 8) + 1;
-            //let activePunctID = Math.floor(Math.random() * 5) + 1;
             let activeVerbID = Math.floor(Math.random() * 48) + 1;
-            //let activeAdjID = Math.floor(Math.random() * 19) + 1;
-            //let activeAdvID = Math.floor(Math.random() * 20) + 1;
+            let activeAdjID = Math.floor(Math.random() * 13) + 1;
    
         // if ID is in tile color array, find card w/matching ID & assign color-line class, else scan next array
             if (yellowTiles.includes(i)) {
@@ -183,39 +152,35 @@ console.log(dataIntjArray);
         // if ID is in tilePOS array, select random word + syllable count from list & return to card w/matching ID, else scan next array
             if (aTiles.includes(i)) {
                 document.getElementById(["tw-" + i]).innerText = "a";
-                //document.getElementById(["ts-" + i]).innerText = 1;
             } else if (verbTiles.includes(i)) {
                 document.getElementById(["tw-" + i]).innerText = dataVerbsArray[activeVerbID];
-               // document.getElementById(["tw-" + i]).innerText = ambDataAdv[activeAdvID].word;
-                //document.getElementById(["ts-" + i]).innerText = ambDataAdv[activeAdvID].wordSyl;
             } else if (betweenTiles.includes(i)) {
                 document.getElementById(["tw-" + i]).innerText = "between";
-                //document.getElementById(["ts-" + i]).innerText = ambDataVerb[activeVerbID].wordSyl;
-            } else if (nounOneTiles.includes(i)) {
-                document.getElementById(["tw-" + i]).innerText = dataNounsArray[activeNounID];
-                //document.getElementById(["ts-" + i]).innerText = ambDataAdj[activeAdjID].wordSyl;
+            } else if (youTiles.includes(i)) {
+                document.getElementById(["tw-" + i]).innerText = "you";
             } else if (andTiles.includes(i)) {
                 document.getElementById(["tw-" + i]).innerText = "and";
-               //document.getElementById(["ts-" + i]).innerText = ambDataNoun[activeNounID].wordSyl;
-            } else if (nounTwoTiles.includes(i)) {
+            } else if (yourTiles.includes(i)) {
+                document.getElementById(["tw-" + i]).innerText = "your";
+            } else if (adjTiles.includes(i)) {
+                document.getElementById(["tw-" + i]).innerText = dataAdjArray[activeAdjID];
+            } else if (nounTiles.includes(i)) {
                 document.getElementById(["tw-" + i]).innerText = dataNounsArray[activeNounID];
-                //document.getElementById(["ts-" + i]).innerText = ambDataIntj[activeIntjID].wordSyl;
-            } else if (intjTiles.includes(i)) {
-                document.getElementById(["tw-" + i]).innerText = dataIntjArray[activeIntjID];
-                //document.getElementById(["ts-" + i]).innerText = ambDataPunct[activePunctID].wordSyl;
-            } else if (breakTiles.includes(i)) {
-                let breakBtn = document.createElement("button");
-                breakBtn.innerHTML = "break"
-                breakBtn.setAttribute("type" , "button");
-                breakBtn.className = "btn btn-outline-dark btn-lg break-btn";
-                breakBtn.setAttribute("data-bs-toggle" , "modal");
-                breakBtn.setAttribute("data-bs-target" , "#modal-" + i);
-                document.getElementById(["btn-break-div-" + i]).appendChild(breakBtn);
-            }
-        }, i * 1000);
+
+        }}, i * 1000);
     });
 };
 
 
-setTimeout(popGrid, 20000)
+setTimeout(popGrid, 20000);
 
+
+/* 
+else if (breakTiles.includes(i)) {
+    let breakBtn = document.createElement("button");
+    breakBtn.innerHTML = "break"
+    breakBtn.setAttribute("type" , "button");
+    breakBtn.className = "btn btn-outline-dark btn-lg break-btn";
+    breakBtn.setAttribute("data-bs-toggle" , "modal");
+    breakBtn.setAttribute("data-bs-target" , "#modal-" + i);
+    document.getElementById(["btn-break-div-" + i]).appendChild(breakBtn); */
