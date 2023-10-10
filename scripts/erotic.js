5c015cab6b97c38c8e1811d5edb50218273849df
EMA <elizabeth.alexander@northwestern.edu>
2023-10-10 07:55:21 -0400

    renamed file and added new json

---
+++ scripts/erotic.js
@@ -0,0 +1,181 @@
+let lindaBelmarLorde = [
+    {
+        "knew" : [
+            "about bundling up against the wicked cold",
+            "about Paradise Plums--hard, oval candies, cherry-red on one side, pineapple-yellow on the other",
+            "which West Indian markets along Lenox Avenue carried [Paradise Plums] in tilt-back glass jars on the countertops",
+            "how desirable Paradise Plums were to sweet-starved little children...in maintaining discipline on long shopping journeys",
+            "exactly how many of the imported goodies could be sucked and rolled around in the mouth before the wicked gum arabic with its acidic british teeth cut through the tongue's pink coat and raised little red pimples",
+            "about mixing oils for bruises and rashes",
+            "about disposing of all toenail clippings and hair from the comb",
+            "about burning candles before All Saint's Day to keep the soucoyants away, lest they suck the blood of her babies",
+            "about blessing the food and yourself before eating",
+            "about saying prayers before going to sleep",
+            "how to frighten children into behaving in public",
+            "how to pretend that the only food left in the house was a meal of choice, carefully planned",
+            "how to make virtues out of necessities",
+            "about food",
+            "that it was a good place to take children if you wanted them to grow up smart",
+            "how to look into people's faces and tell what they were going to do before they did it",
+            "which grapefruit was shaddock and pink, before it ripened, and what to 	do with the others, which was to throw them to the pigs. Except she had no pigs in Harlem, and sometimes those were the only grapefruit around to eat",
+            "how to prevent infection in an open cut or wound by heating the black-elm leaf over a wood-fire until it wilted in the hand, rubbing the juice into the cut, and then laying the soft green now flabby fibers over the wound for a bandage",
+            "to forbid her children to ever set foot into any candystore",
+            "green things were precious, and the peaceful, healing qualities of water" 
+        ]
+    },
+    {
+        "missed" : [
+            "the bashing of the waves against the sea wall at the foot of Noel's Hill the humped and mysterious slope of Marquis Island rising up from the water a half-mile off-shore",
+           "the swift-flying bananaquits and the trees and the rank smell of the tree-ferns lining the road downhill into Grenville town",
+            "the music that did not have to be listened to because it was always around",
+           "the Sunday-long boat trips that took her to Aunt Anni's in Carriacou",
+            "the music",
+            "the annoyance of the early Saturday morning customers with their loose talk and slurred rhythms, warbling home from the rumshop"
+        ]
+    },
+    {
+        "home" : [
+            "she would tell us wonderful stories about Noel's Hill in Grenville, Grenada, which overlooked the Caribbean",
+            "she told us stories about Carriacou, where she had been born, amid the heavy smell of limes",
+            "she told us about plants that healed and about plants that drove you crazy",
+            "she told us about the trees and fruits and flowers that grew outside the door of the house where she grew up and lived until she married",
+            "she breathed exuded hummed the fruit smell of Noel's Hill morning fresh and noon hot, and I spun visions of sapadilla and mango as a net over my Harlem tenement cot in the snoring darkness rank with nightmare sweat"
+        ]
+    }
+];
+
+let deLois = {
+        "lived" : "up the block on 142nd Street",
+        "never" : "had her hair done",
+        "all" : "the neighborhood women sucked their teeth as she walked by",
+        "twinkled" : "[her crispy hair] in the summer sun",
+        "moved" : "[her big proud stomach] on down the block while I watched,",
+        "caring" : "[not] caring wiether or not she was a poem",
+        "tied": "[I] my shoes",
+        "peep" : "[I] under her blouse as she passed by",
+        "loved" : "[her] because she moved like she felt like she was somebody special, like somebody I'd like to know someday"
+};
+
+
+let lindaKnew = [];
+
+for (let index = 0; index < lindaBelmarLorde.length; index++) {
+    lindaKnew.push(lindaBelmarLorde.knew[index]);
+    console.log(lindaKnew);
+};
+
+let lindaMissed = [];
+
+for (let index = 0; index < lindaBelmarLorde.length; index++) {
+    lindaKnew.push(lindaBelmarLorde.missed[index]);
+    console.log(lindaMissed);
+};
+
+let carriacou = [];
+
+for (let index = 0; index < lindaBelmarLorde.length; index++) {
+    lindaKnew.push(lindaBelmarLorde.home[index]);
+    console.log(carriacou);
+};
+
+let deLoisImg = [];
+
+for (let index = 0; index < deLois.length; index++) {
+    deLoisImg.push(deLois[index])
+    console.log(deLoisImg);
+};
+
+let ambSongs = {
+    "Janelle Monáe": [
+        "Crazy, Classic, Life",
+        "Pynk",
+        "Float ft. Seun Kuti & Egypt 80"
+    ],
+    "Jucee Froot" : "Down In The Valley (P-Valley Theme)",
+    "Megan Thee Stallion": [
+        "Savage (Remix) ft. Beyoncé",
+        "Thot Shit"
+    ],
+    "Alex Isley" : "Still Wonder ft. Jack Dine & Robert Glasper",
+}
+
+let chatGPTPrompts = [
+        "Identify the part of speech for each word in the following list",
+        "Identify the number of syllables in every word or phrase in the following list"
+]
+
+let dirtyComputer = [
+    "Dirty Computer",
+    "Crazy, Classic Life",
+    "Take a Byte",
+    "Jane's Dream",
+    "Screwed",
+    "Django Jane",
+    "Pynk",
+    "Make Me Feel",
+    "I Got the Juice",
+    "I Like That",
+    "Don't Judge Me",
+    "Stevie's Dream",
+    "So Afraid",
+    "Americans"
+]
+
+let theAgeOfPleasure = [
+    "Float",
+    "Champagne Shit",
+    "Black Sugar Beach",
+    "Phenomenal",
+    "Haute",
+    "Ooh La La",
+    "Lipstick Lover",
+    "The Rush",
+    "The French 75",
+    "Water Slide",
+    "Know Better",
+    "Paid in Pleasure",
+    "Only Have Eyes 42",
+    "A Dry Red"
+]
+
+
+
+let lipstickLover = {
+    "i" : [
+        "like lipstick on my neck",
+        "wanna feel your lips on mine",
+        "just wanna feel a little tongue",
+        "seen it from the back and I'm excited",
+        "can write it",
+        "will take my time",
+        "really got a thing for my lipstick lover",
+        "will do anything for my lipstick lover"
+    ]
+}
+
+let pynk = [
+    "like the inside of your, baby",
+    "behind all of the doors, crazy",
+    "like the tongue that goes down, maybe",
+    "like the paradise found",
+    "when you're blushing inside, baby",
+    "the truth you can't hide, maybe",
+    "like the folds of your brain, crazy",
+    "as we all go insane",
+    "like the lips around your, maybe",
+    "like the skin that's under, baby",
+    "where it's deepest inside, crazy",
+    "beyond forest and thighs",
+    "like the secrets you hide, maybe",
+    "like the lid of your eye, baby",
+    "is where all of it starts, crazy",
+    "like the holes of your heart",
+    "like the inside of your, baby",
+    "like the walls and the doors, maybe",
+    "like your fingers in my, maybe",
+    "the truth you can't hide",
+    "like your tongue going 'round, baby",
+    "like the sun going down, maybe",
+    "like the holes in your heart, baby",
+    "my favorite part"
+]
